06f8d24cc478eaa63a3308bd21dc23ce641efe7d
Mattie Zilaie <your_name@domain.comAccount>
2019-10-09 13:05:52 -0700

    ]prompts the user for an integer, user will try to guess. higher or lower than the target number, program will output that it is too high or low. Then program will print how many guesses it took

--- num_guess.py
+++ num_guess.py
@@ -7,13 +7,13 @@
 num_guesses = 0
 print('Enter the number for the player to guess.')
 number = int(input())
-print(number)
+
 print('Enter your guess.')
 guess = int()
 
 while guess != number:
     guess = input()
-    print(guess)
+
     guess = int(guess)
     num_guesses = num_guesses +1
 
