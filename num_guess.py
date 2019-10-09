df2016f2e6c3b2c3a5e84772d8b7a71c39090442
Mattie Zilaie <your_name@domain.comAccount>
2019-10-09 12:50:43 -0700

    ]prompts the user for an integer, user will try to guess. higher or lower than the target number, program will output that it is too high or low. Then program will print how many guesses it took

---
+++ num_guess.py
@@ -0,0 +1,23 @@
+# Author: Mahtab Zilaie
+# Date: 10/9/19
+# Description: prompts the user for an integer, user will try to guess
+# higher or lower than the target number, program will output that it is
+# too high or low. Then program will print how many guesses it took
+
+num_guesses = 0
+number = int(input('Enter the number for the player to guess.'))
+print(number)
+print('Enter your guess.')
+guess = int()
+
+while guess != number:
+    guess = input()
+    guess = int(guess)
+    num_guesses = num_guesses +1
+
+    if guess < number:
+        print('Too low - try again:')
+    if guess > number:
+        print('Too high - try again:')
+    if guess == number:
+        print('You guessed it in', num_guesses,'tries.')
